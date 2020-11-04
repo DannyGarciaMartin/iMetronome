29a2535fd5bc6b931ac5e92fed3ee3541744de65
danny martin <danny.garcia.martin@gmail.com>
2020-11-04 00:32:22 -0600

    add local server IMPORTANT NOT TO USE WITH PRODUCTION...

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    host: 'localhost',
+    port: '9000',
+    open: true,
+    compress: true,
+    hotOnly: true,
+    inline: true,
+    lazy: true,
+    filename: 'main.js'
+  }
 };
